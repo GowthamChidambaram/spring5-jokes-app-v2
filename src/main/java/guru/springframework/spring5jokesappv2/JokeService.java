3654c368b7973c71d44bf93a4dea909c87f902a5
Gowtham-chidambaranathan <gowtham.chidambaranathan@freshworks.com>
2021-05-08 20:22:59 +0530

    service layer implemented

---
+++ src/main/java/guru/springframework/spring5jokesappv2/JokeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring5jokesappv2;
+
+/**
+ * Created by Gowtham C on 08/05/21.
+ */
+public interface JokeService {
+
+  String getJoke();
+}
---
+++ src/main/java/guru/springframework/spring5jokesappv2/JokeServiceImpl.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring5jokesappv2;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Gowtham C on 08/05/21.
+ */
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+  private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+  public JokeServiceImpl() {
+    this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+  }
+
+  @Override
+  public String getJoke() {
+    return chuckNorrisQuotes.getRandomQuote();
+
+  }
+}
